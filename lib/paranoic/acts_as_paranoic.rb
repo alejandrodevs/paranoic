2f531e26827cabf14db2c834742c49abd4b6efa8
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2013-10-15 13:15:57 -0500

    Fixing ActsAsParanoic module, adding ClassMethods and InstanceMethods modules

--- lib/paranoic/acts_as_paranoic.rb
+++ lib/paranoic/acts_as_paranoic.rb
@@ -2,26 +2,31 @@ module Paranoic
   module ActsAsParanoic
     autoload :Sanitizer, 'paranoic/acts_as_paranoic/sanitizer'
 
-    def acts_as_paranoic *args
-      @acts_as_paranoic_options  = args.extract_options!
-      @acts_as_paranoic_resource = args.last
-      @acts_as_paranoic = true
-      before_filter :sanitize_params_paranoically
+    module ClassMethods
+      def acts_as_paranoic *args
+        @acts_as_paranoic_options  = args.extract_options!
+        @acts_as_paranoic_resource = args.last
+        @acts_as_paranoic = true
+        before_filter :sanitize_params_paranoically
+      end
+
+      def acts_as_paranoic?
+        @acts_as_paranoic
+      end
     end
 
-    def acts_as_paranoic?
-      @acts_as_paranoic
-    end
-
-    def sanitize_params_paranoically
-      raise "You must implement the current_user method" unless self.respond_to?(:current_user)
-      sanitizer = Sanitizer.new
-      sanitizer.sanitize!(current_user, params, @acts_as_paranoic_resource)
+    module InstanceMethods
+      def sanitize_params_paranoically
+        raise "You must implement the current_user method" unless self.respond_to?(:current_user)
+        sanitizer = Sanitizer.new
+        sanitizer.sanitize!(current_user, params, @acts_as_paranoic_resource)
+      end
     end
 
   end
 end
 
 if defined?(ActionController::Base)
-  ActionController::Base.extend Paranoic::ActsAsParanoic
+  ActionController::Base.send(:extend, Paranoic::ActsAsParanoic::ClassMethods)
+  ActionController::Base.send(:include, Paranoic::ActsAsParanoic::InstanceMethods)
 end
